0cc634fa722694f20872100237a041ea8b0a5d30
saron-solomon <it.saron.solomon@gmail.com>
2021-02-06 07:50:31 +0300

    Date column added Sorting

--- TaskManager ID/assets/js/app.js
+++ TaskManager ID/assets/js/app.js
@@ -1,51 +1,43 @@
-// Define UI Variables 
-const taskInput = document.querySelector('#task'); //the task input text field
-const form = document.querySelector('#task-form'); //The form at the top
-const filter = document.querySelector('#filter'); //the task filter text field
-const taskList = document.querySelector('.collection'); //The UL
-const clearBtn = document.querySelector('.clear-tasks'); //the all task clear button
+const taskInput = document.querySelector('#task');
+const form = document.querySelector('#task-form');
+const filter = document.querySelector('#filter');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
 
-const reloadIcon = document.querySelector('.fa'); //the reload button at the top navigation 
-
-//DB variable 
+const reloadIcon = document.querySelector('.fa');
+const ascending = document.querySelector('.ascending');
+const descending = document.querySelector('.descending');
 
 let DB;
 
-
-
-// Add Event Listener [on Load]
 document.addEventListener('DOMContentLoaded', () => {
-    // create the database
+
     let TasksDB = indexedDB.open('tasks', 1);
 
-    // if there's an error
-    TasksDB.onerror = function() {
-            console.log('There was an error');
-        }
-        // if everything is fine, assign the result to the instance
-    TasksDB.onsuccess = function() {
-        // console.log('Database Ready');
+    TasksDB.onerror = function (e) {
+        console.log(e);
+    }
 
-        // save the result
+    TasksDB.onsuccess = function () {
+
         DB = TasksDB.result;
-
-        // display the Task List 
         displayTaskList();
     }
 
-    // This method runs once (great for creating the schema)
-    TasksDB.onupgradeneeded = function(e) {
-        // the event will be the database
-        let db = e.target.result;
+    TasksDB.onupgradeneeded = function (e) {
+        DB = TasksDB.result;
+        if (!DB.objectStoreNames.contains('tasks')) {
+            tasks = DB.createObjectStore('tasks', { keyPath: 'id', autoIncrement: true });
+            tasks.createIndex('taskname', 'taskname', { unique: false });
+            console.log('Database ready and fields created!');
+        } else {
+            tasks = TasksDB.transaction.objectStore('tasks');
+            console.log('Database ready!');
+        }
 
-        // create an object store, 
-        // keypath is going to be the Indexes
-        let objectStore = db.createObjectStore('tasks', { keyPath: 'id', autoIncrement: true });
-
-        // createindex: 1) field name 2) keypath 3) options
-        objectStore.createIndex('taskname', 'taskname', { unique: false });
-
-        console.log('Database ready and fields created!');
+        if (!tasks.indexNames.contains('date')) {
+            tasks.createIndex('date', 'date');
+        }
     }
 
     form.addEventListener('submit', addNewTask);
@@ -53,25 +45,22 @@ document.addEventListener('DOMContentLoaded', () => {
     function addNewTask(e) {
         e.preventDefault();
 
-        // Check empty entry
         if (taskInput.value === '') {
             taskInput.style.borderColor = "red";
 
             return;
         }
 
-        // create a new object with the form info
         let newTask = {
             taskname: taskInput.value,
+            date: Date.now()
         }
 
-        // Insert the object into the database 
         let transaction = DB.transaction(['tasks'], 'readwrite');
         let objectStore = transaction.objectStore('tasks');
 
         let request = objectStore.add(newTask);
 
-        // on success
         request.onsuccess = () => {
             form.reset();
         }
@@ -85,38 +74,33 @@ document.addEventListener('DOMContentLoaded', () => {
         }
 
     }
+    let reverseOrder = false;
+
 
     function displayTaskList() {
-        // clear the previous task list
         while (taskList.firstChild) {
             taskList.removeChild(taskList.firstChild);
         }
 
-        // create the object store
         let objectStore = DB.transaction('tasks').objectStore('tasks');
+        let index = objectStore.index('date');
+        let req = index.openCursor(null, reverseOrder ? 'prev' : 'next');
 
-        objectStore.openCursor().onsuccess = function(e) {
-            // assign the current cursor
+        req.onsuccess = function (e) {
             let cursor = e.target.result;
 
             if (cursor) {
 
-                // Create an li element when the user adds a task 
                 const li = document.createElement('li');
-                //add Attribute for delete 
                 li.setAttribute('data-task-id', cursor.value.id);
-                // Adding a class
                 li.className = 'collection-item';
-                // Create text node and append it 
                 li.appendChild(document.createTextNode(cursor.value.taskname));
-                // Create new element for the link 
                 const link = document.createElement('a');
-                // Add class and the x marker for a 
                 link.className = 'delete-item secondary-content';
                 link.innerHTML = `
                  <i class="fa fa-remove"></i>
                 &nbsp;
-                <a href="/Lesson 04 [Lab 06]/Finished/edit.html?id=${cursor.value.id}"><i class="fa fa-edit"></i> </a>
+                <a href="edit.html?id=${cursor.value.id}"><i class="fa fa-edit"></i> </a>
                 `;
                 // Append link to li
                 li.appendChild(link);
@@ -162,6 +146,18 @@ document.addEventListener('DOMContentLoaded', () => {
         tasks.clear();
         displayTaskList();
         console.log("Tasks Cleared !!!");
+    }
+
+    ascending.addEventListener('click', sortByDate);
+    descending.addEventListener('click', sortByDate);
+
+    function sortByDate() {
+        reverseOrder = !reverseOrder;
+        displayTaskList();
+
+        ascending.classList.toggle("disabled");
+        descending.classList.toggle("disabled");
+
     }
 
 
